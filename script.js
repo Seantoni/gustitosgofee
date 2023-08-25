8d2dbf39bdbfe04c76b011bbfa0d6fe8f813d988
Seantoni <101011785+Seantoni@users.noreply.github.com>
2023-08-25 15:43:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function createChart(results) {
                 fill: 'origin'
             },
             {
-                label: 'Ingreso SimpleGo',
+                label: 'Ingreso por Go',
                 data: dataNuevoIngresoBruto,
                 backgroundColor: 'rgba(255, 159, 64, 0.2)',
                 borderColor: 'rgba(255, 159, 64, 1)',
